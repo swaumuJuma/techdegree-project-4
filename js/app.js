b3ffc8e5361049bc3a384f1ee1e897d8ee6c1f21
swaumu <swaumumagai@gmail.com>
2019-08-29 21:10:35 +0300

    comments; disable tab focus
    Added comments;
    Phase displayed when game lost;
    disable tab focus on onscreen keyboard;

--- js/app.js
+++ js/app.js
@@ -6,31 +6,30 @@ let game;
 
 document.querySelector('#btn__reset').addEventListener('click', () => {  // Reveal game board when Sart Game is clicked
     game = new Game();
-    game.startGame();    
+    game.startGame();
 });
 
 document.querySelector('#qwerty').addEventListener('click', e => {    // When any letter is clicked, handleInteractions will be called
     if(e.target.className === 'key') {
         game.handleInteraction(e.target);
     }
-})
+});
 
 let keys = [];
 keys = document.querySelectorAll('.key');
 keys.forEach(key => key.setAttribute('tabindex', -1));      // Disable tab key focus on onscreen keyboard
 
 document.addEventListener('keydown', e => {          // EXTRA CREDIT: Add functionality to physical keyboard
-    const letter = event.key.toLowerCase(); 
-    const valid = /^[A-Za-z]$/.test(letter);        // Check to see if a single letter was entered 
+    const letter = event.key.toLowerCase();
+    const valid = /^[A-Za-z]$/.test(letter);        // Check to see if a single letter was entered
     const overlay = document.querySelector('#overlay').style.display;  // check to see if game has started
-  
+
     if (valid && overlay === 'none') {              // If single letter was entered, game has started, and key has not been disabled, handle interactions
-        keys.forEach(key => { 
+        keys.forEach(key => {
             keyLetter = key.textContent;
-            if(letter.includes(keyLetter) && key.disabled !== true) { 
+            if(letter.includes(keyLetter) && key.disabled !== true) {
                 game.handleInteraction(key);
             }
         });
     }
 });
-
